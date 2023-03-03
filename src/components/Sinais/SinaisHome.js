b655277a86b309f17066d441f499ca45dc02b0c6
abudluan <abudluan@gmail.com>
2023-03-03 11:53:28 -0300

    feat: SinaisHome grid added

--- src/components/Sinais/SinaisHome.js
+++ src/components/Sinais/SinaisHome.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { MDBContainer, MDBNavbar, MDBNavbarBrand, MDBIcon } from 'mdb-react-ui-kit';
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBIcon,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn
+} from 'mdb-react-ui-kit';
 import { Link } from 'react-router-dom';
 
 const SinaisHome = () => {
@@ -14,6 +27,79 @@ const SinaisHome = () => {
                     </MDBNavbarBrand>
                 </MDBContainer>
             </MDBNavbar>
+
+            <MDBContainer className="my-5 text-center">
+                <MDBRow>
+                    <MDBCol size='md'>
+                        <MDBCard>
+                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                            <MDBCardBody>
+                                <MDBCardTitle>Card title</MDBCardTitle>
+                                <MDBCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </MDBCardText>
+                                <MDBBtn href='#'>Button</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol size='md'>
+                        <MDBCard>
+                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                            <MDBCardBody>
+                                <MDBCardTitle>Card title</MDBCardTitle>
+                                <MDBCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </MDBCardText>
+                                <MDBBtn href='#'>Button</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol size='md'>
+                        <MDBCard>
+                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                            <MDBCardBody>
+                                <MDBCardTitle>Card title</MDBCardTitle>
+                                <MDBCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </MDBCardText>
+                                <MDBBtn href='#'>Button</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+
+                <MDBRow className="my-5 text-center">
+                <MDBCol md='2'>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                    <MDBCard>
+                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                            <MDBCardBody>
+                                <MDBCardTitle>Card title</MDBCardTitle>
+                                <MDBCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </MDBCardText>
+                                <MDBBtn href='#'>Button</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                    <MDBCard>
+                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+                            <MDBCardBody>
+                                <MDBCardTitle>Card title</MDBCardTitle>
+                                <MDBCardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </MDBCardText>
+                                <MDBBtn href='#'>Button</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+
+                </MDBRow>
+
+            </MDBContainer>
+
         </section>
     );
 };
